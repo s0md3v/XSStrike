f2c23dea90c8b89ef5ba998aa30309580caf2591
Somdev Sangwan <s0md3v@gmail.com>
2018-10-27 21:58:49 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+/*
+	Tessellate by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 1000px)',
+		mobile: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Scrolly links.
+			$('.scrolly').scrolly();
+
+		// Prioritize "important" elements on narrow.
+			skel.on('+narrow -narrow', function() {
+				$.prioritize(
+					'.important\\28 narrow\\29',
+					skel.breakpoint('narrow').active
+				);
+			});
+
+	});
+
+})(jQuery);
