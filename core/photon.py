72d1807aed99e9aa5a4c0767679673555224d2ff
Somdev Sangwan <s0md3v@gmail.com>
2018-10-28 14:57:36 +0530

    added -l option to control crawling depth

--- core/photon.py
+++ core/photon.py
@@ -8,31 +8,16 @@ from core.zetanize import zetanize
 from core.requester import requester
 from core.utils import getUrl, getParams
 
-def photon(main_url, url, headers):
-    urls = set() # urls found
+def photon(seedUrl, headers, level):
     forms = [] # web forms
     processed = set() # urls that have been crawled
     storage = set() # urls that belong to the target i.e. in-scope
-    host = urlparse(url).netloc
-    url = getUrl(url, '', True)
-    schema = urlparse(main_url).scheme
-    params = getParams(url, '', True)
-    response = requester(url, params, headers, True, 0).text
-    forms.append(zetanize(response))
-    matches = findall(r'<[aA].*href=["\']{0,1}(.*?)["\']', response)
-    for link in matches: # iterate over the matches
-        link = link.split('#')[0] # remove everything after a "#" to deal with in-page anchors
-        if link[:4] == 'http':
-            if link.startswith(main_url):
-                urls.add(link)
-        elif link[:2] == '//':
-            if link.split('/')[2].startswith(host):
-                urls.add(schema + link)
-        elif link[:1] == '/':
-            urls.add(main_url + link)
-        else:
-            urls.add(main_url + '/' + link)
+    schema = urlparse(seedUrl).scheme
+    host = urlparse(seedUrl).netloc
+    main_url = schema + '://' + host
+    storage.add(seedUrl)
     def rec(target):
+        processed.add(target)
         print ('%s Parsing %s' % (run, target))
         url = getUrl(target, '', True)
         params = getParams(target, '', True)
@@ -43,9 +28,24 @@ def photon(main_url, url, headers):
             forms.append({0: {'action': url, 'method': 'get', 'inputs': inps}})
         response = requester(url, params, headers, True, 0).text
         forms.append(zetanize(response))
-    from core.config import threadCount
-    threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=threadCount)
-    futures = (threadpool.submit(rec, url) for url in urls)
-    for i, _ in enumerate(concurrent.futures.as_completed(futures)):
-        pass
+        matches = findall(r'<[aA].*href=["\']{0,1}(.*?)["\']', response)
+        for link in matches: # iterate over the matches
+            link = link.split('#')[0] # remove everything after a "#" to deal with in-page anchors
+            if link[:4] == 'http':
+                if link.startswith(main_url):
+                    storage.add(link)
+            elif link[:2] == '//':
+                if link.split('/')[2].startswith(host):
+                    storage.add(schema + link)
+            elif link[:1] == '/':
+                storage.add(main_url + link)
+            else:
+                storage.add(main_url + '/' + link)
+    for x in range(level):
+        urls = storage - processed
+        from core.config import threadCount
+        threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=threadCount)
+        futures = (threadpool.submit(rec, url) for url in urls)
+        for i, _ in enumerate(concurrent.futures.as_completed(futures)):
+            pass
     return forms
