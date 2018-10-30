0dfb25bc506af76d35399b077fc3229e96eb1f9c
Somdev Sangwan <s0md3v@gmail.com>
2018-10-30 12:49:36 +0530

    better regex for locating sources

--- core/dom.py
+++ core/dom.py
@@ -5,16 +5,16 @@ def dom(response, silent=False):
     result = False
     highlighted = []
     response = response.split('\n')
-    SOURCES_RE = r"""location\.|\.([.\[]\s*["']?\s*arguments|dialogArguments|innerHTML|write|open|showModalDialog|cookie|URL|documentURI|baseURI|referrer|name|opener|parent|top|content|self|frames)\b|(localStorage|sessionStorage|Database)\b"""
-    SINKS_RE = r"""( (src|href|data|location|code|value|action)=)|(replace|assign|navigate|getResponseHeader|open|showModalDialog|eval|evaluate|execCommand|execScript|setTimeout|setInterval)\("""
+    sources = r"""location\.|\.([.\[]\s*["']?\s*arguments|dialogArguments|innerHTML|write|open|showModalDialog|cookie|URL|documentURI|baseURI|referrer|name|opener|parent|top|content|self|frames)[^\w\-]|(localStorage|sessionStorage|Database)[^\w\-]"""
+    sinks = r"""( (src|href|data|location|code|value|action)=)|(replace|assign|navigate|getResponseHeader|open|showModalDialog|eval|evaluate|execCommand|execScript|setTimeout|setInterval)\("""
     num = 1
     for newLine in response:
         line = newLine
-        pattern = re.findall(SOURCES_RE, line)
+        pattern = re.findall(sources, line)
         for grp in pattern:
             source = ''.join(grp)
             line = line.replace(source, yellow + source + end)
-        pattern = re.findall(SINKS_RE, line)
+        pattern = re.findall(sinks, line)
         for grp in pattern:
             sink = ''.join(grp)
             line = line.replace(sink, red + sink + end)
