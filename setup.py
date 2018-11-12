bd2edc83261a40217d3f9412c7690429998fc7cc
Mert Akengin <mert.akengin@iyzico.com>
2018-11-12 17:53:56 +0300

    added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='XSStrike',
+    version='1.0',
+    packages=['core'],
+    url='https://somdev.me/XSStrike',
+    license='GPLv3',
+    author='s0md3v',
+    author_email='s0md3v@gmail.com',
+    description='Most advanced XSS detection suite.'
+)
