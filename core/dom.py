35a11487f544e9f9d206688c9a744756d338177e
Somdev Sangwan <s0md3v@gmail.com>
2018-11-12 23:31:57 +0530

    potential fix for #93

--- core/dom.py
+++ core/dom.py
@@ -7,17 +7,20 @@ def dom(response):
     sources = r"""location\.|\.([.\[]\s*["']?\s*arguments|dialogArguments|innerHTML|write|open|showModalDialog|cookie|URL|documentURI|baseURI|referrer|name|opener|parent|top|content|self|frames)[^\w\-]|(localStorage|sessionStorage|Database)[^\w\-]"""
     sinks = r"""( (src|href|data|location|code|value|action)=)|(replace|assign|navigate|getResponseHeader|open|showModalDialog|eval|evaluate|execCommand|execScript|setTimeout|setInterval)\("""
     num = 1
-    for newLine in response:
-        line = newLine
-        pattern = re.findall(sources, line)
-        for grp in pattern:
-            source = ''.join(grp)
-            line = line.replace(source, yellow + source + end)
-        pattern = re.findall(sinks, line)
-        for grp in pattern:
-            sink = ''.join(grp)
-            line = line.replace(sink, red + sink + end)
-        if line != newLine:
-            highlighted.append('%-3s %s' % (str(num), line.lstrip(' ')))
-        num += 1
+    try:
+        for newLine in response:
+            line = newLine
+            pattern = re.findall(sources, line)
+            for grp in pattern:
+                source = ''.join(grp)
+                line = line.replace(source, yellow + source + end)
+            pattern = re.findall(sinks, line)
+            for grp in pattern:
+                sink = ''.join(grp)
+                line = line.replace(sink, red + sink + end)
+            if line != newLine:
+                highlighted.append('%-3s %s' % (str(num), line.lstrip(' ')))
+            num += 1
+    except MemoryError:
+        pass
     return highlighted
