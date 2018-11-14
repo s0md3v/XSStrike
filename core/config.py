959523cae9d438d802cffaffc043fa92008e19fa
Somdev Sangwan <s0md3v@gmail.com>
2018-11-14 10:45:59 +0530

    Add files via upload

--- core/config.py
+++ core/config.py
@@ -1,8 +1,11 @@
-changes = '''blind xss support'''
+changes = '''proxy support'''
 
 defaultEditor = 'nano'
 blindPayload = '' # your blind XSS payload
 xsschecker = 'v3dm0s' # A non malicious string to check for reflections and stuff
+
+# More information on adding proxies: http://docs.python-requests.org/en/master/user/advanced/#proxies
+proxies = {'http' : 'http://0.0.0.0:8080', 'https' : 'http://0.0.0.0:8080'}
 
 minEfficiency = 90
 
