dada14f3e80687ed7664820423e5360684df6f81
Devin Stokes <rune.dev@gmail.com>
2019-04-30 17:52:44 -0700

    make the browser static so we don't have to initialize it each execution.

--- core/browserEngine.py
+++ core/browserEngine.py
@@ -1,18 +1,17 @@
 import re
 import os
 import sys
-from core.config import xsschecker
 from core.utils import writer
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.webdriver.support import expected_conditions as EC
 
+options = Options()
+options.add_argument('--headless')
+browser = webdriver.Firefox(options=options)
 
 def browserEngine(response):
-    options = Options()
-    options.add_argument('--headless')
-    browser = webdriver.Firefox(options=options)
     response = re.sub(r'<script.*?src=.*?>', '<script src=#>', response, re.I)
     response = re.sub(r'href=.*?>', 'href=#>', response, re.I)
     writer(response, 'test.html')
@@ -27,7 +26,6 @@ def browserEngine(response):
         if EC.alert_is_present():
             popUp = True
         browser.quit()
-
 
     except UnexpectedAlertPresentException:
         popUp = True
