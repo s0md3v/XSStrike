98f3c2cd77aa486a37b2cf844a76a466b68995d9
sunny <p@git>
2019-11-27 14:42:38 +0330

    PEP20: Complex is better than complicated./

--- core/requester.py
+++ core/requester.py
@@ -1,12 +1,13 @@
 import random
-import requests
 import time
-from urllib3.exceptions import ProtocolError
 import warnings
 
+import requests
+from urllib3.exceptions import ProtocolError
+
 import core.config
-from core.utils import converter, getVar
 from core.log import setup_logger
+from core.utils import converter, getVar
 
 logger = setup_logger(__name__)
 
@@ -24,9 +25,7 @@ def requester(url, data, headers, GET, delay, timeout):
     user_agents = ['Mozilla/5.0 (X11; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0',
                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
                    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991']
-    if 'User-Agent' not in headers:
-        headers['User-Agent'] = random.choice(user_agents)
-    elif headers['User-Agent'] == '$':
+    if ('User-Agent' not in headers) or (headers['User-Agent'] == '$'):
         headers['User-Agent'] = random.choice(user_agents)
     logger.debug('Requester url: {}'.format(url))
     logger.debug('Requester GET: {}'.format(GET))
